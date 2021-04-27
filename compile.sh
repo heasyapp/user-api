32fac329f2a5d33abd9de85698dd868ffb463b15
Juno Roesler <healtheasy.app@gmail.com>
2021-04-27 19:28:32 -0300

    Initial Commit

---
+++ compile.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+appname=$(cat pom.xml | grep \<artifactId\> | head -1 | cut -d \> -f 2 | cut -d \< -f 1)
+appversion=$(cat pom.xml | grep \<version\> | head -1 | cut -d \> -f 2 | cut -d \< -f 1)
+docker run -i -v /var/run/docker.sock:/var/run/docker.sock \
+  -v $(pwd):/src \
+  -v $HOME/.m2:/root/.m2 \
+  -e DOCKER_IMAGE="heasyapp/$appname:$appversion" heasyapp/builder:latest
